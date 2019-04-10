3ee1e3aa9060913d1a10159d89cf86aa25730f2f
SaRang <ipcp365@gmail.com>
2019-04-10 15:05:55 +0900

    하단풋터 작성 1,2,4페이지에 해당하는 xml작성 (04.10 수정본)

--- app/src/main/java/com/example/alone_project/mypage.java
+++ app/src/main/java/com/example/alone_project/mypage.java
@@ -5,41 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class mypage extends AppCompatActivity {
 
     Button footer_menu1;
     Button footer_menu4;
+    Button footer_menu2;
 
     View.OnClickListener footer_menu1_1 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent alone_life_intent = new Intent(MainActivity.this, alone_life.class);
+            Intent alone_life_intent = new Intent(mypage.this, alone_life.class);
             startActivity(alone_life_intent);
         }
     };
 
-    View.OnClickListener footer_menu4_4 = new View.OnClickListener() {
+    View.OnClickListener footer_menu2_2 = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent mypage_intent = new Intent(MainActivity.this, mypage.class);
-            startActivity(mypage_intent);
+            Intent main_intent = new Intent(mypage.this, MainActivity.class);
+            startActivity(main_intent);
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mypage);
 
         footer_menu1 = (Button)findViewById(R.id.footer_menu1);
         footer_menu1.setOnClickListener(footer_menu1_1);
 
-        footer_menu4 = (Button)findViewById(R.id.footer_menu4);
-        footer_menu4.setOnClickListener(footer_menu4_4);
-
-
-
+        footer_menu2 = (Button)findViewById(R.id.footer_menu2) ;
+        footer_menu2.setOnClickListener(footer_menu2_2);
     }
 }
