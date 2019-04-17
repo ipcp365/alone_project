506bba969cfd42bad97496b6bfaa343c27f77008
sarang <ipcp365@gmail.com>
2019-04-17 19:17:34 +0900

    뷰페이저 넣기!!

--- app/src/main/java/com/example/alone_project/MainActivity.java
+++ app/src/main/java/com/example/alone_project/MainActivity.java
@@ -1,85 +1,88 @@
 package com.example.alone_project;
 
-import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    Button bt1,bt2,bt3,bt4;
-    FragmentManager fm;
-    FragmentTransaction tran;
-    Frag1 frag1;
-    Frag2 frag2;
-    Frag3 frag3;
-    Frag4 frag4;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    Frag1 blankfragment1;
+    Frag2 blankfragment2;
+    Frag3 blankfragment3;
+    Frag4 blankfragment4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
 
-        bt1 = (Button) findViewById(R.id.bt1);
-        bt2 = (Button) findViewById(R.id.bt2);
-        bt3 = (Button) findViewById(R.id.bt3);
-        bt4 = (Button) findViewById(R.id.bt4);
+        blankfragment1 = new Frag1();
+        blankfragment2 = new Frag2();
+        blankfragment3 = new Frag3();
+        blankfragment4 = new Frag4();
 
-        bt1.setOnClickListener(this);
-        bt2.setOnClickListener(this);
-        bt3.setOnClickListener(this);
-        bt4.setOnClickListener(this);
+        MypagerAdapter mypagerAdapter = new MypagerAdapter(getSupportFragmentManager());
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.setAdapter(mypagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
 
-        frag1 = new Frag1(); //프래그먼트 객채셍성
-        frag2 = new Frag2(); //프래그먼트 객채셍성
-        frag3 = new Frag3(); //프래그먼트 객채셍성
-        frag4 = new Frag4();
-        setFrag(0); //프래그먼트 교체
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
-    @Override
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.bt1:
-                setFrag(0);
-                break;
-            case R.id.bt2:
-                setFrag(1);
-                break;
-            case R.id.bt3:
-                setFrag(2);
-                break;
-            case R.id.bt4:
-                setFrag(3);
-                break;
+
+    class MypagerAdapter extends FragmentPagerAdapter {
+
+        String[] fragmentname = {"Frag1", "Frag2", "Frag3", "Frag4"};
+
+        public MypagerAdapter(FragmentManager fr) {
+            super(fr);
         }
-    }
-    public void setFrag(int n){    //프래그먼트를 교체하는 작업을 하는 메소드를 만들었습니다
-       //  fm = getFragmentManager();
-       fm = getSupportFragmentManager();
-        tran = fm.beginTransaction();
-        switch (n){
-            case 0:
-                tran.replace(R.id.main_frame, frag1);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
-            case 1:
-                tran.replace(R.id.main_frame, frag2);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
-            case 2:
-                tran.replace(R.id.main_frame, frag3);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
 
-            case 3:
-                tran.replace(R.id.main_frame, frag4);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    return blankfragment1;
+                case 1:
+                    return blankfragment2;
+                case 2:
+                    return blankfragment3;
+                case 3:
+                    return blankfragment4;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentname.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentname[position];
         }
     }
 }
-
